2f2107abf692ca7df871ea7f47fa98533ffa4cc4
SUS0a <tom969805235@163. com>
2018-08-26 23:33:34 +0800

    fork

---
+++ spring-restful-authorization-master/src/main/java/com/scienjus/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.scienjus.repository;
+
+import com.scienjus.domain.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+/**
+ * User类的CRUD操作
+ * @see com.scienjus.domain.User
+ * @author ScienJus
+ * @date 2015/7/10.
+ */
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    public User findByUsername(String username);
+
+    public List<User> findAll();
+}
